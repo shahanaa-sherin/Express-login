e4c9967e515e6ba143b42d815112659172fdf81d
shahanaa-sherin <shahanasherin@gmail.com>
2024-06-24 11:45:21 +0530

    remove errors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require('path')
+const bodyparser = require('body-parser')
+const session = require('express-session')
+const {v4:uuidv4} = require('uuid');
 const app = express();
 
 const port = process.env.PORT ||8080;
@@ -9,6 +12,13 @@ app.set('view engine','ejs')
 //load static assests
 app.use('/static',express.static(path.join(__dirname,'public')))
 app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
 //home route
 
 app.get('/',(req,res)=>{
